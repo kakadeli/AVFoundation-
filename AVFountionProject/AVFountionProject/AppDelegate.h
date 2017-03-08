c4c9a535affbbd6cda5e39d128a75c39d4a90ce9
lijinhua <929691052@qq.com>
2017-03-08 16:49:38 +0800

    这次行不行

---
+++ AVFountionProject/AVFountionProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVFountionProject
+//
+//  Created by lijinhua on 2017/3/8.
+//  Copyright © 2017年 lijinhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVFountionProject/AVFountionProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFountionProject
+//
+//  Created by lijinhua on 2017/3/8.
+//  Copyright © 2017年 lijinhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
